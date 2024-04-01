c41886f6fb91be83f15e69974a9162995a36fb98
yanagitani <takanori.yanagitani@gmail.com>
2024-04-02 08:06:21 +0900

    test: more coverage

--- flat/flatjson2json_test.go
+++ flat/flatjson2json_test.go
@@ -7,6 +7,9 @@ import (
 	"encoding/json"
 	"os"
 
+	"bytes"
+	"fmt"
+
 	fj "github.com/takanoriyanagitani/go-json2json/flat"
 )
 
@@ -28,4 +31,28 @@ func ExampleConvKeys_Convert() {
 		log.Fatal(e)
 	}
 	// Output: {"name":"JD","seqno":42}
+}
+
+func ExampleIoJSON2JSON_ConvertAll() {
+	var buf bytes.Buffer
+	var dec *json.Decoder = json.NewDecoder(&buf)
+	var enc *json.Encoder = json.NewEncoder(os.Stdout)
+	ck := fj.ConvKeys{Keys: []string{"seqno", "name"}}
+	var cv fj.Converter = ck.AsIf()
+	_, _ = fmt.Fprintf(&buf, `
+		{"seqno":42,"name":"JD","phone":"01-2345-6789"}
+		{"seqno":43,"name":"DJ","phone":"ab-cdef-0123"}
+	`)
+	ij2j := fj.IoJSON2JSON{
+		Decoder:   dec,
+		Converter: cv,
+		Encoder:   enc,
+	}
+	e := ij2j.ConvertAll(context.Background())
+	if nil != e {
+		log.Fatal(e)
+	}
+	// Output:
+	// {"name":"JD","seqno":42}
+	// {"name":"DJ","seqno":43}
 }
